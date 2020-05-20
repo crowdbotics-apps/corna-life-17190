a6f58ded132ef19cda70e8d0da7da877ae721f48
Crowdbotics <team@crowdbotics.com>
2020-05-20 15:28:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/corna_life_17190/MainActivity.java
@@ -0,0 +1,15 @@
+package com.corna_life_17190;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "corna_life_17190";
+    }
+}
